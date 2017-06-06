3584d1d56ef1e91eeb3f79d613532b7bea0cc1ef
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-06-06 14:35:48 +0100

    Weather terms fix
    Simple descriptions added to case statments

--- public/js/weatherTypeCreate.js
+++ public/js/weatherTypeCreate.js
@@ -11,8 +11,10 @@ function weatherTypeCreate(string)
 	{
 		case "light drizzle":
 		case "heavy drizzle":
+		case "drizzle":
 		case "light rain":
 		case "heavy rain":
+		case "rain":
 		{
 			wtc.dataWeatherType 	= "rain";
 			wtc.dataDisplay 		= "rain";
@@ -24,14 +26,19 @@ function weatherTypeCreate(string)
 		case "heavy snow":
 		case "light snow grains":
 		case "heavy snow grains":
+		case "snow":
 		case "light ice crystals":
 		case "heavy ice crystals":
+		case "ice crystals":
 		case "light ice pellets":
 		case "heavy ice pellets":
+		case "ice pellets":
 		case "light low drifting snow":
 		case "heavy low drifting snow":
+		case "drifting snow":
 		case "light blowing snow":
 		case "heavy blowing snow":
+		case "blowing snow":
 		{
 			wtc.dataWeatherType 	= "snow";
 			wtc.dataDisplay 		= "snow";
@@ -42,6 +49,7 @@ function weatherTypeCreate(string)
 		case "light hail":
 		case "heavy hail":
 		case "small hail":
+		case "hail":
 		{
 			wtc.dataWeatherType 	= "hail";
 			wtc.dataDisplay 		= "hail";
@@ -51,6 +59,7 @@ function weatherTypeCreate(string)
 
 		case "light mist":
 		case "heavy mist":
+		case "mist":
 		{
 			wtc.dataWeatherType 	= "mist";
 			wtc.dataDisplay 		= "mist";
@@ -60,8 +69,10 @@ function weatherTypeCreate(string)
 
 		case "light fog":
 		case "heavy fog":
+		case "fog":
 		case "light fog patches":
 		case "heavy fog patches":
+		case "fog patches":
 		{
 			wtc.dataWeatherType 	= "fog";
 			wtc.dataDisplay 		= "fog";
@@ -71,6 +82,7 @@ function weatherTypeCreate(string)
 
 		case "light smoke":
 		case "heavy smoke":
+		case "smoke":
 		{
 			wtc.dataWeatherType 	= "smoke";
 			wtc.dataDisplay 		= "smoke";
@@ -80,6 +92,7 @@ function weatherTypeCreate(string)
 
 		case "light volcanic ash":
 		case "heavy volcanic ash":
+		case "volcanic ash":
 		{
 			wtc.dataWeatherType 	= "volcano";
 			wtc.dataDisplay 		= "volcano";
@@ -89,6 +102,8 @@ function weatherTypeCreate(string)
 
 		case "light widespread dust":
 		case "heavy widespread dust":
+		case "widespread dust":
+		case "dust":
 		{
 			wtc.dataWeatherType 	= "dust";
 			wtc.dataDisplay 		= "dust";
@@ -98,12 +113,17 @@ function weatherTypeCreate(string)
 
 		case "light sand":
 		case "heavy sand":
+		case "sand":
 		case "light sandstorm":
 		case "heavy sandstorm":
+		case "sandstorm":
 		case "light low drifting sand":
 		case "heavy low drifting sand":
+		case "low drifting sand":
+		case "drifting sand":
 		case "light blowing sand":
 		case "heavy blowing sand":
+		case "blowing sand":
 		{
 			wtc.dataWeatherType 	= "sandstorm";
 			wtc.dataDisplay 		= "sandstorm";
@@ -113,6 +133,7 @@ function weatherTypeCreate(string)
 
 		case "light haze":
 		case "heavy haze":
+		case "haze":
 		{
 			wtc.dataWeatherType 	= "haze";
 			wtc.dataDisplay 		= "haze";
@@ -122,6 +143,7 @@ function weatherTypeCreate(string)
 
 		case "light spray":
 		case "heavy spray":
+		case "spray":
 		{
 			wtc.dataWeatherType 	= "spray";
 			wtc.dataDisplay 		= "spray";
@@ -131,10 +153,14 @@ function weatherTypeCreate(string)
 
 		case "light dust whirls":
 		case "heavy dust whirls":
+		case "dust whirls":
 		case "light low drifting widespread dust":
 		case "heavy low drifting widespread dust":
+		case "low drifting widespread dust":
 		case "light blowing widespread dust":
 		case "heavy blowing widespread dust":
+		case "blowing widespread dust":
+		case "widespread dust":
 		{
 			wtc.dataWeatherType 	= "dust+storm";
 			wtc.dataDisplay 		= "dust storm";
@@ -145,6 +171,7 @@ function weatherTypeCreate(string)
 
 		case "light rain mist":
 		case "heavy rain mist":
+		case "rain mist":
 		{
 			wtc.dataWeatherType 	= "mist";
 			wtc.dataDisplay 		= "mist";
@@ -154,6 +181,7 @@ function weatherTypeCreate(string)
 
 		case "light rain showers":
 		case "heavy rain showers":
+		case "rain showers":
 		{
 			wtc.dataWeatherType 	= "rain+showers";
 			wtc.dataDisplay 		= "rain showers";
@@ -163,6 +191,7 @@ function weatherTypeCreate(string)
 
 		case "light snow showers":
 		case "heavy snow showers":
+		case "snow showers":
 		{
 			wtc.dataWeatherType 	= "snow+showers";
 			wtc.dataDisplay 		= "snow showers";
@@ -172,6 +201,9 @@ function weatherTypeCreate(string)
 
 		case "light snow blowing snow mist":
 		case "heavy snow blowing snow mist":
+		case "snow blowing snow mist":
+		case "snow blowing":
+		case "snow mist":
 		{
 			wtc.dataWeatherType 	= "snow+mist";
 			wtc.dataDisplay 		= "snow mist";
@@ -181,10 +213,13 @@ function weatherTypeCreate(string)
 
 		case "light ice pellet showers":
 		case "heavy ice pellet showers":
+		case "ice pellet showers":
 		case "light hail showers":
 		case "heavy hail showers":
+		case "hail showers":
 		case "light small hail showers":
 		case "heavy small hail showers":
+		case "small hail showers":
 		{
 			wtc.dataWeatherType 	= "hail+showers";
 			wtc.dataDisplay 		= "hail showers";
@@ -194,16 +229,22 @@ function weatherTypeCreate(string)
 
 		case "light thunderstorm":
 		case "heavy thunderstorm":
+		case "thunderstorm":
 		case "light thunderstorms and rain":
 		case "heavy thunderstorms and rain":
+		case "thunderstorms and rain":
 		case "light thunderstorms and snow":
 		case "heavy thunderstorms and snow":
+		case "thunderstorms and snow":
 		case "light thunderstorms and ice pellets":
 		case "heavy thunderstorms and ice pellets":
+		case "thunderstorms and ice pellets":
 		case "light thunderstorms with hail":
 		case "heavy thunderstorms with hail":
+		case "thunderstorms with hail":
 		case "light thunderstorms with small hail":
 		case "heavy thunderstorms with small hail":
+		case "thunderstorms with small hail":
 		{
 			wtc.dataWeatherType 	= "thunderstorm";
 			wtc.dataDisplay 		= "thunderstorm";
@@ -213,8 +254,10 @@ function weatherTypeCreate(string)
 
 		case "light freezing drizzle":
 		case "heavy freezing drizzle":
+		case "freezing drizzle":
 		case "light freezing rain":
 		case "heavy freezing rain":
+		case "freezing rain":
 		{
 			wtc.dataWeatherType 	= "freezing+rain";
 			wtc.dataDisplay 		= "freezing rain";
@@ -224,6 +267,7 @@ function weatherTypeCreate(string)
 
 		case "light freezing fog":
 		case "heavy freezing fog":
+		case "freezing fog":
 		{
 			wtc.dataWeatherType 	= "freezing+fog";
 			wtc.dataDisplay 		= "freezing fog";
@@ -234,6 +278,7 @@ function weatherTypeCreate(string)
 		case "patches of fog":
 		case "shallow fog":
 		case "partial fog":
+		case "fog":
 		{
 			wtc.dataWeatherType 	= "fog";
 			wtc.dataDisplay 		= "fog";
@@ -259,7 +304,9 @@ function weatherTypeCreate(string)
 
 		case "partly cloudy":
 		case "mostly cloudy":
+		case "cloudy":		
 		case "scattered clouds":
+		case "clouds":
 		{
 			wtc.dataWeatherType 	= "cloudy";
 			wtc.dataDisplay 		= "cloudy";
